ce0f352b2503a32f1aedf3272992b90831435605
Zaenal Arif <zaenalariftech@gmail.com>
2025-01-21 09:55:47 +0700

    Change utils to extension function

--- app/src/main/java/com/mobbelldev/diaryapp/utils/FormatDate.kt
+++ app/src/main/java/com/mobbelldev/diaryapp/utils/FormatDate.kt
@@ -3,15 +3,13 @@ package com.mobbelldev.diaryapp.utils
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-object DateUtils {
-    fun formatDate(inputDate: String): String {
-        try {
-            val inputFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
-            val outputFormat = SimpleDateFormat("EEE, dd MM yyyy", Locale.ENGLISH)
-            val date = inputFormat.parse(inputDate)
-            return date?.let { outputFormat.format(it) } ?: "Invalid Date"
-        } catch (e: Exception) {
-            return "Invalid Date"
-        }
+fun String.convertDate(): String {
+    return try {
+        val inputFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+        val outputFormat = SimpleDateFormat("EEE, dd MM yyyy", Locale.ENGLISH)
+        val date = inputFormat.parse(this)
+        date?.let { outputFormat.format(it) } ?: "Invalid Date"
+    } catch (e: Exception) {
+        "Invalid Date"
     }
 }
